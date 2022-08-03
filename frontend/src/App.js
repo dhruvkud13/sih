877c0c09b03fd8c6d6268de568ac71c5549c20bf
INSAEN <pratham1986@gmail.com>
2022-08-03 14:53:20 +0530

    backend also

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
+    <Navbar/>
       <Login/>
     </div>
   );
