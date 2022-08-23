70a1b52ec245142d8939e59e5c98195cd682424e
dhruvkud13 <dhruvkudalkar13@gmail.com>
2022-08-23 16:03:22 +0530

    delete

--- frontend/src/components/FileTable.js
+++ frontend/src/components/FileTable.js
@@ -8,49 +8,74 @@ import { setModal } from "../redux/fileModalSlice.js";
 import { useSelector } from "react-redux";
 import { FileView } from "./FileViewer";
 import "./FileTable.css";
+import differenceBy from 'lodash/differenceBy';
+import OurButton from "./OurButton";
 
 function FileTable() {
-  const [fileData, setFileData] = useState([]);
+  const [fileData, setFileData] = useState(data);
   const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    const url = "http://localhost:3000/getallfiles";
-    const fetchData = async () => {
-      try {
-        const response = await fetch(url);
-        const json = await response.json();
-        for (const file in json) {
-          if(file.fileVisibility)
-          setFileData([...fileData, file]);
-        }
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchData();
-    // console.log(data); //why is this empty?
-  }, []);
+  const [selectedRows, setSelectedRows] = React.useState([]);
+	const [toggleCleared, setToggleCleared] = React.useState(false);
+  // useEffect(() => {
+  //   const url = "http://localhost:3000/getallfiles";
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch(url);
+  //       const json = await response.json();
+  //       for (const file in json) {
+  //         if(file.fileVisibility)
+  //         setFileData([...fileData, file]);
+  //       }
+  //       setLoading(false);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
   const tableData = {
     columns,
-    data,
+    fileData,
   };
+  const handleRowSelected = React.useCallback(state => {
+		setSelectedRows(state.selectedRows);
+	}, []);
+  const contextActions = React.useMemo(() => {
+		const handleDelete = () => {
+			
+			if (window.confirm(`Are you sure you want to delete:\r ${selectedRows.map(r => r.title)}?`)) {
+				setToggleCleared(!toggleCleared);
+				setFileData(differenceBy(fileData, selectedRows, 'title'));
+			}
+		};
+    console.log(fileData);
+
+		return (
+			<OurButton onClick={handleDelete} title="Delete" />
+		);
+	}, [fileData, selectedRows, toggleCleared]);
   const dispatch = useDispatch();
   const modal = useSelector((state) => state.modal);
   return (
     <div className="">
       <div className="">
-        <DataTableExtensions {...tableData}>
+        {/* <DataTableExtensions {...tableData}> */}
           <DataTable
+            title="Documents"
             columns={columns}
-            data={data}
-            noHeader
+            data={fileData}
+            // noHeader
             defaultSortField="id"
             defaultSortAsc={false}
             pagination
             highlightOnHover
             onRowClicked={() => dispatch(setModal(true))}
+            selectableRows
+			      contextActions={contextActions}
+			      onSelectedRowsChange={handleRowSelected}
+			      clearSelectedRows={toggleCleared}
           />
-        </DataTableExtensions>
+        {/* </DataTableExtensions> */}
       </div>
       {modal.isModal ? <FileView type={"pdf"}/> : <div></div>}
     </div>
