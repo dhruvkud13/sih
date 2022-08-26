a070d4b2a909f6edbebca450e009b5cb4ecb2e28
yashr10 <yashr10102@gmail.com>
2022-08-26 10:33:56 +0530

    make optimization changes

--- frontend/src/components/AppliedSch.js
+++ frontend/src/components/AppliedSch.js
@@ -1,55 +1,59 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+import { setModal } from '../redux/fileModalSlice';
 import { ExclamationCircleOutlined } from '@ant-design/icons';
 import { Button, Modal, Space } from 'antd';
+import { FileView } from './FileViewer';
 const { confirm } = Modal;
 
 
 
 const AppliedSch = (props) => {
+  const dispatch = useDispatch();
   const [loading, setLoading] = useState(false);
   const [visible, setVisible] = useState(false);
   const user = useSelector((state) => state.user);
-  const handleApprove=async(e)=>{
-    const url="http://localhost:8000/approvescholar";
-    try{
-      const email=props.email;
-      const scholarshipID=props.no
+  const modal = useSelector((state) => state.modal);
+  const handleApprove = async (e) => {
+    const url = "http://localhost:8000/approvescholar";
+    try {
+      const email = props.email;
+      const scholarshipID = props.no
       console.log(scholarshipID);
-        const body={email,scholarshipID};
-        console.log(JSON.stringify(body))
-        await fetch(url,{
-          method:"POST",
-          headers:{"Content-Type":"application/json"},
-          body:JSON.stringify(body),
-        }).then((res)=>{
-          return res.json();
-        }).then((data)=>{
-          console.log(data);
-        })
-    }catch(err){
+      const body = { email, scholarshipID };
+      console.log(JSON.stringify(body))
+      await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      }).then((res) => {
+        return res.json();
+      }).then((data) => {
+        console.log(data);
+      })
+    } catch (err) {
       console.log(err);
     }
   }
-  const handleReject=async(e)=>{
-    const url="http://localhost:8000/rejectscholar";
-    try{
-      const email=props.email;
-      const scholarshipID=props.no
+  const handleReject = async (e) => {
+    const url = "http://localhost:8000/rejectscholar";
+    try {
+      const email = props.email;
+      const scholarshipID = props.no
       console.log(scholarshipID);
-        const body={email,scholarshipID};
-        console.log(JSON.stringify(body))
-        await fetch(url,{
-          method:"POST",
-          headers:{"Content-Type":"application/json"},
-          body:JSON.stringify(body),
-        }).then((res)=>{
-          return res.json();
-        }).then((data)=>{
-          console.log(data);
-        })
-    }catch(err){
+      const body = { email, scholarshipID };
+      console.log(JSON.stringify(body))
+      await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      }).then((res) => {
+        return res.json();
+      }).then((data) => {
+        console.log(data);
+      })
+    } catch (err) {
       console.log(err);
     }
   }
@@ -58,8 +62,8 @@ const AppliedSch = (props) => {
       title: 'Do you want to approve or dissapprove?',
       icon: <ExclamationCircleOutlined />,
       content: `CGPA: ${props.cgpa} , Colllege name: ${props.coll}`,
-      okText:"Approve",
-      cancelText:"Reject",
+      okText: "Approve",
+      cancelText: "Reject",
       onOk() {
         console.log('OK');
         handleApprove();
@@ -94,15 +98,24 @@ const AppliedSch = (props) => {
     //   </Modal>
   };
   return (
-    <div onClick={showConfirm}>
-        <div className="rounded-xl shadow-md px-5 py-3 font-raleway flex flex-col">
-      
-      <div className="font-bold text-[18px]">{props.name}</div>
-      <div>{props.date}</div>
-      <div>{props.degree}</div>
-      <div className='flex flex-row'>Status: {props.status}</div>
-      <div className="text-txtgrey"></div>
-  </div>
+    <div >
+      <div className="rounded-xl shadow-md px-5 py-3 font-raleway flex flex-col">
+        <div onClick={() => { dispatch(setModal(true)) }}>View Marksheet</div>
+        <div className="font-bold text-[18px]">{props.name}</div>
+        <div>{props.date}</div>
+        <div>{props.degree}</div>
+        <div  className='flex flex-row'>Status: {props.status}</div>
+        <div onClick={showConfirm} className="text-txtgrey">Decide</div>
+      </div>
+      {modal.isModal ? (
+        <FileView
+          type={"pdf"}
+          // rellink={row.hash}
+          rellink="QmSFr7mfrbiijCTCT8kw3vuqHg2xq86uJkHomY21KLkfNA"
+        />
+      ) : (
+        <div></div>
+      )}
     </div>
   )
 }
